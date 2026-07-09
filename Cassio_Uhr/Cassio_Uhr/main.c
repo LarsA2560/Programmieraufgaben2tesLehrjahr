cb6e8fd1d15b064070291edfb475b18ca1fcd964
LarsA2560 <lars.maerki@stud.msw.ch>
2026-07-09 15:45:36 +0200

    hinzufügen des Zustandswechsels

--- Cassio_Uhr/Cassio_Uhr/main.c
+++ Cassio_Uhr/Cassio_Uhr/main.c
@@ -19,11 +19,13 @@
 #define MASK_IN_TASTER_FUNCTION (1<<6)
 #define SONNTAG (6)
 #define MONTAG (0)
+#define AlARM (7)
+#define STOPPUHR (8)
 
 #define MASK_OUT_MAX_BRIGHTNESS_LCD (255)
 
-#define MASK_SCHRITTGROSSE_s (60)
-#define MASK_SCHRITTGROSSE_m (60)
+#define MASK_SCHRITTGROSSE_s (1)
+#define MASK_SCHRITTGROSSE_m (1)
 #define MASK_SCHRITTGROSSE_h (1)
 #define MASK_SCHRITTGROSSE_d (1)
 #define MASK_SCHRITTGESCHWINDIGKEIT_ms (1000)
@@ -42,6 +44,9 @@ int main(void)
     char * MMM[3] = { "AM ","PM ","24h"};
     char * DD[9] = { "MO","TU","WE","TH","FR","SA","SO","AL","ST"};
     const uint8_t zwolfSunden_UHR[24] = { 120,10,20,30,40,50,60,70,80,90,100,110,121,11,21,31,41,51,61,71,81,91,101,111};
+    typedef enum zustand_mode_t        {NORMALZEITANZEIGE, TAGLICHER_ALARM, STOPPUHRFUNKTION, ZEIT_KALENDER_EINSTELLFUNKTION}             zustand_mode_t;
+    zustand_mode_t zustand_mode = NORMALZEITANZEIGE;
+        
     uint8_t dd = 0;
     uint16_t LAP = 0;
     uint8_t hh = 0;
@@ -52,6 +57,7 @@ int main(void)
     uint8_t flagBeepEin = 0;
     uint8_t flag_function = 0;
     uint8_t Wochentag = 0;
+    uint8_t Wochentag_Ausgabe = 0;
     
     uint8_t inTaster = 0;
     uint8_t inTaster_alt = 0;
@@ -82,60 +88,85 @@ int main(void)
         systemZeit_ms = getSystemTimeMs();     
         //Verarbeitung_______________________________________________________________________________________________________________________________________________________          
         hh = hh_24;
+        Wochentag_Ausgabe = Wochentag;
         if (inTaster_Light)
         {
             outLCDbrightness = !outLCDbrightness;
         }
+        if (inTaster_Mode)
+        {
+            zustand_mode += 1;
+            if (zustand_mode > 3)
+            {
+                zustand_mode = NORMALZEITANZEIGE;
+            }
+        }
         
-        if (inTaster_Function)
+        switch (zustand_mode)
         {
-            if (flag_function==2)
+        case NORMALZEITANZEIGE:
+            if (inTaster_Function)
             {
-                flag_function = 0;
+                if (flag_function==2)
+                {
+                    flag_function = 0;
+                }
+                else
+                {
+                    flag_function = 2;
+                }
             }
-            else
+            if (flag_function<2)
             {
-                flag_function = 2;
-            }
-        }
-        if (flag_function<2)
-        {
-            hh = zwolfSunden_UHR[hh_24];
-            flag_function = (hh % 2);
-            hh = hh/10;
+                hh = zwolfSunden_UHR[hh_24];
+                flag_function = (hh % 2);
+                hh = hh/10;
             
-        }
-        else{
-            hh = hh_24;
-        }
-        if((systemZeit_ms - sekunde) >= MASK_SCHRITTGESCHWINDIGKEIT_ms){
-            ss += MASK_SCHRITTGROSSE_s;
-            sekunde = systemZeit_ms;
-        }
-        if (ss>= 60)
-        {
-            ss = 0;
-            mm += MASK_SCHRITTGROSSE_m;
-        }
-        if (mm>=60)
-        {
-            mm = 0;
-            hh_24 += MASK_SCHRITTGROSSE_h;
-        }
-        if (hh_24 >= 24)
-        {
-            hh_24 = 0;
-            dd += MASK_SCHRITTGROSSE_d;
-            Wochentag += 1;
-        }
-        if (Wochentag == SONNTAG)
-        {
-            Wochentag = MONTAG;
-        }
-             
+            }
+            else{
+                hh = hh_24;
+            }
+        
+            if((systemZeit_ms - sekunde) >= MASK_SCHRITTGESCHWINDIGKEIT_ms){
+                ss += MASK_SCHRITTGROSSE_s;
+                sekunde = systemZeit_ms;
+            }
+            if (ss>= 60)
+            {
+                ss = 0;
+                mm += MASK_SCHRITTGROSSE_m;
+            }
+            if (mm>=60)
+            {
+                mm = 0;
+                hh_24 += MASK_SCHRITTGROSSE_h;
+            }
+            if (hh_24 >= 24)
+            {
+                hh_24 = 0;
+                dd += MASK_SCHRITTGROSSE_d;
+                Wochentag += 1;
+            }
+            if (Wochentag == SONNTAG)
+            {
+                Wochentag = MONTAG;
+                Wochentag_Ausgabe = Wochentag;
+            }
+        	break;
+        case TAGLICHER_ALARM:
+            Wochentag_Ausgabe = AlARM; 
+            break;
+        case STOPPUHRFUNKTION:
+            Wochentag_Ausgabe = STOPPUHR;
+            
+            break;
+        case ZEIT_KALENDER_EINSTELLFUNKTION:
+            
+            break;
+    	}     
         //Ausgabe____________________________________________________________________________________________________________________________________________________________  
         lcdLight(outLCDbrightness*MASK_OUT_MAX_BRIGHTNESS_LCD);
-        lcdWriteText(0,0,"%s %s %2u %3u",MMM[flag_function],DD[Wochentag],dd,LAP);
+        lcdWriteText(0,0,"%s %s %2u %3u",MMM[flag_function],DD[Wochentag_Ausgabe],dd,LAP);
         lcdWriteText(1,0,"%2u:%02u:%02u",hh,mm,ss);
         //lcdWriteText(2,0,"%c %c", );     
         //Warten_____________________________________________________________________________________________________________________________________________________________
